e35512cd9d7859cf52ef582bb3d9c21de3630636
Anna-Gladush <ann.gladush@gmail.com>
2026-02-26 16:21:40 +0200

    Can add new books to library

--- scripts/index.js
+++ scripts/index.js
@@ -21,14 +21,13 @@ function addBookToLibrary(title, author, year, pages, read) {
 
 function displayBook(book) {
   const card = document.createElement('div');
-  const btn = document.createElement('btn');
   let read;
   if (book.read == "Read") {
       read = '<button class="btn read">Unread</button>'
     } else {
       read = '<button class="btn">Read</button>'
     }
-  card.innerHTML = `<h1>${book.title}</h1> <p>${book.author}</p> <p>${book.year}</p> <p>${book.pages} pages</p> ${read}`;
+  card.innerHTML = `<div class="card"><h1>${book.title}</h1> <p>${book.author}</p> <p>${book.year}</p> <p>${book.pages} pages</p> ${read} </div>`;
   card.classList.add('.card');
   div.appendChild(card);
 }
@@ -48,17 +47,28 @@ function readTheBook(book) {
 }
 
 function displayAddNewBook() {
-  
+  const btn = document.querySelector('.submit');
+  let title = document.getElementById('title');
+  let author = document.getElementById('author');
+  let year = document.getElementById('year');
+  let pages = document.getElementById('pages');
+
+  btn.addEventListener('click', () => {
+    let read = document.getElementById('read').checked === true ? "Read" : "Unread";
+    addBookToLibrary(title.value, author.value, year.value, pages.value, read);
+    displayLibrary();
+  });
 }
 function displayLibrary() {
-  addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 310, 'Read');
-  addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1954, 1500);
-  addBookToLibrary("Pride and Prejudice", "Jane Austen", 1813, 328);
-
+  div.innerHTML = '';
   myLibrary.forEach((book) => {
     displayBook(book);
     readTheBook(book);
   })
 }
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 310, 'Read');
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1954, 1500);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 1813, 328);
 
-displayLibrary()
+displayAddNewBook();
+displayLibrary();
