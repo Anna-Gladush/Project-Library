5fafe36f4516ca9a2a4bcfa4f1d3eac7bff5d328
Anna-Gladush <ann.gladush@gmail.com>
2026-02-26 14:20:49 +0200

    Made code for creating new Books and pushing them into array

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    };
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    };
+}
+
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook)
+}
+
+function displayLibrary() {
+  
+}
