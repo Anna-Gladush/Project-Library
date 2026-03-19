aea5d18a7eb346cd771a911da18615dba9c5a395
Anna-Gladush <ann.gladush@gmail.com>
2026-03-19 15:11:18 +0200

    Pushed all library methods into module pattern

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,6 @@
 const div = document.querySelector('.container')
 const myLibrary = [];
 
-// function Book(title, author, year, pages, read = 'Unread') {
-//     if (!new.target) {
-//         throw Error("You must use the 'new' operator to call the constructor");
-//     };
-//     this.title = title;
-//     this.author = author;
-//     this.year = year;
-//     this.id = crypto.randomUUID();
-//     this.pages = pages;
-//     this.read = read;
-// }
-
 class Book {
   constructor(title, author, year, pages, read = 'Unread') {
     this.title = title;
@@ -23,97 +11,108 @@ class Book {
     this.read = read;
   }
 }
-function addBookToLibrary(title, author, year, pages, read) {
-  const newBook = new Book(title, author, year, pages, read);
-  myLibrary.push(newBook)
-}
 
-function displayBook(book) {
-  const card = document.createElement('div');
-  let read;
-  let id = book.id
-  if (book.read == "Read") {
-      read = `<button class="btn read" data-id="${id}">Unread</button>`
-    } else {
-      read = `<button class="btn" data-id="${id}">Read</button>`
-    }
-  card.innerHTML = `<div class="card" data-idr="${id}"><h2>${book.title}</h2> <p>${book.author}</p> <p>${book.year}</p> <p>${book.pages} pages</p> <div class="action">${read}<button class="remove" data-id="${id}">Remove</button></div></div>`;
-  div.appendChild(card);
-}
-Book.prototype.toggleRead =function () {
+Book.prototype.toggleRead = function () {
   this.read = this.read === 'Read' ? 'Unread' : "Read";
 }
-function readTheBook() {
-  const btn = document.querySelectorAll('.btn');
-  btn.forEach((button) => {
-    button.addEventListener('click', () => { 
+
+const Library = (() => {
+  const addBookToLibrary = (title, author, year, pages, read) => {
+    const newBook = new Book(title, author, year, pages, read);
+    myLibrary.push(newBook)
+  }
+  
+  const displayBook = (book) => {
+    const card = document.createElement('div');
+    let read;
+    let id = book.id
+    if (book.read == "Read") {
+        read = `<button class="btn read" data-id="${id}">Unread</button>`
+      } else {
+        read = `<button class="btn" data-id="${id}">Read</button>`
+      }
+    card.innerHTML = `<div class="card" data-idr="${id}"><h2>${book.title}</h2> <p>${book.author}</p> <p>${book.year}</p> <p>${book.pages} pages</p> <div class="action">${read}<button class="remove" data-id="${id}">Remove</button></div></div>`;
+    div.appendChild(card);
+  }  
+
+  const readTheBook = () => {
+    const btn = document.querySelectorAll('.btn');
+    btn.forEach((button) => {
+      button.addEventListener('click', () => { 
+        const bookID =  button.dataset.id;
+        const elBook = myLibrary.find(book => book.id === bookID);
+        if (elBook) {
+          elBook.toggleRead();
+          displayLibrary();
+        }
+        if (button.textContent == 'Read') {
+        button.textContent = 'Unread';
+        button.classList.add('read');
+      } else {
+        button.textContent = 'Read';
+        button.classList.remove('read');
+      }
+    })
+    });
+  }
+
+  const displayAddNewBook = () => {
+    const btn = document.querySelector('.submit');
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let year = document.getElementById('year');
+    let pages = document.getElementById('pages');
+    btn.addEventListener('click', () => {
+      let read = document.getElementById('read');
+      check = read.checked === true ? "Read" : "Unread";
+      addBookToLibrary(title.value, author.value, year.value, pages.value, check);
+      displayLibrary();
+      title.value = '';
+      author.value = '';
+      year.value = '';
+      pages.value = '';
+      read.checked = false;
+    });
+  }
+
+  const displayLibrary = () => {
+    div.innerHTML = '';
+    myLibrary.forEach((book) => {
+      displayBook(book);
+    })
+    readTheBook();
+  }
+
+  const deleteBook = () => {
+    const btn = document.querySelectorAll('.remove');
+    btn.forEach((button) => {
+    button.addEventListener('click', () => {
       const bookID =  button.dataset.id;
-      const elBook = myLibrary.find(book => book.id === bookID);
-      if (elBook) {
-        elBook.toggleRead();
+      console.log(bookID);
+      const idx = myLibrary.findIndex(book => book.id === bookID);
+      if (idx !== -1) {
+        myLibrary.splice(idx, 1);
         displayLibrary();
       }
-      if (button.textContent == 'Read') {
-      button.textContent = 'Unread';
-      button.classList.add('read');
-    } else {
-      button.textContent = 'Read';
-      button.classList.remove('read');
-    }
+    });
+    });
+  }
 
-  })
-  });
-}
-function displayAddNewBook() {
-  const btn = document.querySelector('.submit');
-  let title = document.getElementById('title');
-  let author = document.getElementById('author');
-  let year = document.getElementById('year');
-  let pages = document.getElementById('pages');
-  btn.addEventListener('click', () => {
-    let read = document.getElementById('read');
-    check = read.checked === true ? "Read" : "Unread";
-    addBookToLibrary(title.value, author.value, year.value, pages.value, check);
+  const library = ()=>{
+    displayAddNewBook();
     displayLibrary();
-    title.value = '';
-    author.value = '';
-    year.value = '';
-    pages.value = '';
-    read.checked = false;
-  });
-}
+    deleteBook();
+  }
 
-function displayLibrary() {
-  div.innerHTML = '';
-  myLibrary.forEach((book) => {
-    displayBook(book);
-  })
-  readTheBook();
-}
+  return {
+    addBookToLibrary,
+    library
+  }
+})();
 
-function deleteBook() {
-  const btn = document.querySelectorAll('.remove');
-  btn.forEach((button) => {
-  button.addEventListener('click', () => {
-    const bookID =  button.dataset.id;
-    console.log(bookID);
-    const idx = myLibrary.findIndex(book => book.id === bookID);
-    if (idx !== -1) {
-      myLibrary.splice(idx, 1);
-      displayLibrary();
-    }
-  });
-  });
-}
+Library.addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 310, 'Read');
+Library.addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1954, 1500, 'Unread');
+Library.addBookToLibrary("Pride and Prejudice", "Jane Austen", 1813, 328);
+Library.addBookToLibrary("The Picture of Dorian Gray", "Oscar Wilde", 1890, 230);
 
-function library(){
-  displayAddNewBook();
-  displayLibrary();
-  deleteBook();
-}
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 310, 'Read');
-addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1954, 1500, 'Unread');
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 1813, 328);
-addBookToLibrary("The Picture of Dorian Gray", "Oscar Wilde", 1890, 230);
-
-library()
+Library.library();
